a8e29f8fdfd3f60ba951f80fa8f80a9aa4905db5
C0rbi <allan.nicolas331966@gmail.com>
2026-03-20 12:04:42 +1100

    tkt

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -1,20 +1,23 @@
 extends CharacterBody2D
 
-@export var speed = 100
+@export var speed : float = 100
 var character_direction : Vector2
 
-func _physics_process(delta: float) -> void:
-	character_direction.x = Input.get_axis("left", "right")
-	character_direction.y = Input.get_axis("up", "down")
+func _physics_process(delta):
+	# Utilisation de get_vector pour éviter d'aller trop vite en diagonale
+	character_direction = Input.get_vector("left", "right", "up", "down")
 	
-	if character_direction.x > 0: %sprite.flip_h = false
-	elif character_direction.x < 0: %sprite.flip_h = true
+	if character_direction.x > 0: 
+		%AnimatedSprite2D.flip_h = false
+	elif character_direction.x < 0: 
+		%AnimatedSprite2D.flip_h = true
 	
-	if character_direction:
+	if character_direction != Vector2.ZERO:
 		velocity = character_direction * speed
-		if %sprite.animation != ""
-
-
-
-func player_movement(delta):
-	pass
+		if %AnimatedSprite2D.animation != "right": %AnimatedSprite2D.play("right")
+	else:
+		# Utilise une valeur de friction (ex: 500) au lieu de speed pour un arrêt net
+		velocity = velocity.move_toward(Vector2.ZERO, speed)
+		if %AnimatedSprite2D.animation != "idle": %AnimatedSprite2D.play("idle")
+		
+	move_and_slide()
